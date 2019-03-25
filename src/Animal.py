c08ada9ca0f642fcf0db1444b919c9059be5a050
Carl Flodin <carl.flodin@raa.se>
2019-03-25 17:18:19 +0100

    nytt

--- src/Animal.py
+++ src/Animal.py
@@ -1,14 +1,26 @@
 class Animal:
 
     name = ""
+    sound = ""
 
     def __init__(self):
         pass
 
     def speak(self):
         print(self.name)
+        print(self.sound)
 
 class Dog(Animal):
     def __init__(self, name):
         self.name = name
+        sound = "woff"
+        
+class Cat(Animal):
+    def _init_(self,name):
+        self.name = name
+        sound = "meow"
+        
+
+
+
 
