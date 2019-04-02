979660a08439fca40e31e7dfd74019933a71c9b5
Carl Flodin <carl.flodin@raa.se>
2019-04-02 14:11:06 +0200

    sql

--- SQL/Database.py
+++ SQL/Database.py
@@ -1,5 +1,7 @@
 import sys
 import sqlite3 as sql
+import random, string
+from os import system
 
 
 class DB():
@@ -18,6 +20,9 @@ class DB():
     def closeConnection(self):
         self.con.close()
 
+    def commit(self):
+        self._getConnection().commit()
+
     def _getCursor(self):
         return self._getConnection().cursor()
 
@@ -30,16 +35,24 @@ class DB():
     def dbExecute(self, query):
 
         cur = self._getCursor()
-        cur.execute(query)
+        try:
+            cur.execute(query)
+        except Exception as e:
+            return ["!"]
+
         return cur.fetchall()
 
     def dbExecuteSafe(self, query):
         query = self._sanitize(query)
         self.dbExecute(query)
 
-    def dbExecuteAndPrintSafe(self,query):
+    def dbExecuteAndPrintSafe(self, query):
         query = self._sanitize(query)
-        self._printResult(self.dbExecute(query))
+
+        result = self.dbExecute(query)
+
+        self._printResult(result)
+            
 
     def dbExecuteAndPrint(self, query):
         self._printResult(self.dbExecute(query))
@@ -47,6 +60,7 @@ class DB():
     def dbDropTable(self, tableName):
         tableName = self._sanitize(tableName)
         self.dbExecute(str.format("drop table if exists {}", tableName))
+        self.commit()
 
     def getVersion(self):
         cur = self.getCursor()
@@ -57,12 +71,23 @@ class DB():
         print(str.format("SQLite version: {}", data))
 
     def _printResult(self, result):
-
+        
         for row in result:
             finalStr = ""
             for elem in row:
                 finalStr += str(elem) + ", "
             print(finalStr[:-2])
+
+    def getTableNames(self, table):
+
+        query = str.format("PRAGMA table_info({})", table)
+        data = self.dbExecute(query)
+        returnText = []
+
+        for row in data:
+            returnText.append(row[1])
+
+        return returnText
 
     def _addAllowedChars(self):
         self.allowedChars = {}
@@ -79,14 +104,67 @@ class DB():
                 self.sanitized += c
 
         return self.sanitized
-
+    
 
 ####################
 
+    def genRandomUsers(self, n):
+
+        fNames = ["Ella","Wilma","Ebba","Olivia","Astrid","Alma", "Elsa", "Alice", "Maja", 
+        "Lilly","William","Liam","Noah","Lucas","Oliver","Oscar","Elias","Hugo","Adam","Alexander","Carl"]
+        lNames = ["Smith","Jones","Taylor","Brown","Williams","Wilson","Johnson","Davies","Robinson",
+        "Wright","Thompson","Evans","Walker","White","Roberts","Green","Hall","Jackson","Clarke","Flodin"]
+
+        for i in range(n):
+
+            #if i%10 == 0:
+            #    system(str.format("title {:.3g}%",i/n*100))
+
+            fName = random.choice(fNames)
+            lName = random.choice(lNames)
+            username = fName + self.id_generator(8, string.digits)
+            password = self.id_generator(12)
+            age = random.randint(18,60)
+
+            self.register(username, password, fName, lName, age)
+        
+        self.commit()
+            
+    
+    def id_generator(self, size=6, chars=string.ascii_letters + string.digits):
+        
+        return ''.join(random.choice(chars) for _ in range(size))
+
+
+    def register(self, username, password, fName, lName, age):
+
+        query = str.format("""
+        insert into users (username, password, fName, lName, age) 
+        values
+        ('{0}', '{1}', '{2}', '{3}', {4})
+        """, username, password, fName, lName, age)
+
+        self.dbExecute(query)
+
     def login(self, username, password):
 
-        query = str.format(
-            "select * from users where name = '{0}' and deadliftMax = {1}", username, password)
+        query = str.format("""
+        select * from users where username = '{0}' and password = {1}
+        """, username, password)
+
         print(query)
         self.dbExecuteAndPrint(query)
 
+
+    def createUserTable(self):
+        query = """
+            CREATE TABLE users(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username varchar(16) unique,
+            password varchar(16),
+            fName varchar(8),
+            lName varchar(12),
+            age UNSIGNED TINYINT)"""
+
+        self.dbDropTable('users')
+        self.dbExecute(query)
--- SQL/main.py
+++ SQL/main.py
@@ -1,42 +1,23 @@
 from Database import DB
 from os import system
+import time, string
 
 system("cls")
 
-db = DB("test.db")
+def main():
+    db = DB("test.db")
 
-createUserTable = """
-CREATE TABLE users(
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    name varchar(8),
-    deadliftMax int)
-"""
-getUsers = """
-SELECT * FROM users
-ORDER BY deadliftMax desc
-"""
+    #db.genRandomUsers(200)
 
-db.dbDropTable("users")
-db.dbExecute(createUserTable)
-db.dbExecute("insert into users (name,deadliftMax) values ('Carl', 210)")
-db.dbExecute("insert into users (name,deadliftMax) values ('Carlos', 150)")
-db.dbExecute("insert into users (name,deadliftMax) values ('Cortez', 100)")
-db.dbExecute("insert into users (name,deadliftMax) values ('Charles', 145)")
-db.dbExecute("insert into users (name,deadliftMax) values ('David', 473)")
-db.dbExecute("insert into users (name,deadliftMax) values ('Sputnik', 125)")
-db.dbExecute("insert into users (name,deadliftMax) values ('Al', 170)")
-db.dbExecute("insert into users (name,deadliftMax) values ('Millan', 25)")
-db.dbExecute("insert into users (name,deadliftMax) values ('Hafþór Júlíus Björnsson', 474)")
+    
+    result = db.dbExecute("select count(*) from users")
+    print("Total entries:", result[0][0], end="")
 
-#db.dbExecuteAndPrint(getUsers)
+    #print(db._sanitize("!!''''' or #&/¤%&hej"))
 
-#db.getUserByName("Carl' or 1=1 or '")
+    db.closeConnection()
 
-#db.login("Carl' or 1=1 or 'a'='a", "0")
 
-#db.dbExecuteAndPrintSafe("select * from users order by name limit 5")
 
-for i in range(10):
-    print("Ha Ha")
-
-#print(db._sanitize("!!''''' or #&/¤%&hej"))
+if __name__ == "__main__":
+    main()
